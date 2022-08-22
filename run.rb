6fea809657f353216c2b69f83efbd4523452b0e7
Samuel Banya <sbanya@fastmail.com>
2022-08-22 18:18:32 -0400

    Adding latest changes to assignment

---
+++ run.rb
@@ -0,0 +1,15 @@
+# NOTE: 'Classes' in Ruby are 'Pascal' cased, meaning each word is capitalized
+class Dog
+end
+
+fido = Dog.new
+puts "fido", fido
+
+snoopy = Dog.new
+puts "\nsnoopy", snoopy
+
+lassy = Dog.new
+puts "\nlassy", lassy
+
+class MyClass
+end
